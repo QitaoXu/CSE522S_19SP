4bab2ddd70d5b61b3a497d4acf5498809896d838
徐期涛 <xuqitao@qx-mbp.dhcp.wustl.edu>
2019-02-02 10:59:04 -0600

    studio7_update

--- studios/studio7_io_handling/select.c
+++ studios/studio7_io_handling/select.c
@@ -6,9 +6,35 @@
 #include <sys/time.h>
 #include <sys/types.h>
 
+#define BUF_LEN 1024
 
 int main( int argc, char* argv[] ) {
     
+    int ret_select;
+
+    struct timeval tv;
+    fd_set readfds;
+
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+
+    while (1) {
+
+        ret_select = select(STDIN_FILENO + 1, &readfds, NULL, NULL, NULL);
+
+        if (ret_select < 0) {
+            printf("Error: select() system call failed! Reason: %s\n", strerror(errno));
+            exit(-1);
+        }
+        if (ret_select == 0) continue;
+
+        if (ret_select > 0) {
+
+        }
+
+    }
+
+
     return 0;
 
 }
