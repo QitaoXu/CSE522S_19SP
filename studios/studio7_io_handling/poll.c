ad0f92f9ddd89303426f1309aa84d63e01ebbfb9
徐期涛 <xuqitao@qx-mbp.dhcp.wustl.edu>
2019-02-02 20:54:55 -0600

    studio7_step4

--- studios/studio7_io_handling/poll.c
+++ studios/studio7_io_handling/poll.c
@@ -171,7 +171,7 @@ int main( int argc, char* argv[] ) {
                         //printf("in read while loop\n");
                         ret_read = read(fds[j].fd, buf_socket, BUF_SIZE);
                         //printf("fds[j].fd = %d, ret_read = %d buf_socket: %s\n", fds[j].fd, ret_read, buf_socket);
-                        //sleep(2);
+                        sleep(2);
                         if (ret_read < 0) {
                             printf("Error: read() system call failed! Reason: %s\n", strerror(errno));
                             exit(-1);
