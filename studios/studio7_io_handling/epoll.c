a010912bfc289b7c0dbadf30aaf7ee28d564203d
徐期涛 <xuqitao@qx-mbp.dhcp.wustl.edu>
2019-02-03 19:52:53 -0600

    studio7_step6

---
+++ studios/studio7_io_handling/epoll.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/select.h>
+#include <sys/types.h>
+
+#define BUF_LEN 1024
+#define QUIT "quit"
+
+int main(int argc, char* argv[]) {
+
+    return 0;
+
+    fd_set readfds;
+    int ret_select;
+
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+
+    while (1) {
+        ret_select = select(STDIN_FILENO + 1, &readfds, NULL, NULL, NULL);
+
+        if (ret_select < 0) {
+            printf("Error: select() system call failed! Reason: %s\n", strerror(errno));
+            exit(-1);
+        }
+
+        if (ret_select == 0) continue;
+
+        if (ret_select > 0) {
+
+            if (FD_ISSET(STDIN_FILENO, &readfds)) {
+
+                char buf[BUF_LEN + 1];
+                int len;
+                
+                len = read(STDIN_FILENO, buf, BUF_LEN);
+
+                if (len < 0) {
+                    printf("Error: read() system call failed! Reason: %s\n", strerror(errno));
+                    exit(-1);
+                }
+
+                if (len > 0) {
+                    buf[len] = '\0';
+                    printf("Read from stdin: %s\n", buf);
+                    if (strncmp(buf, QUIT, strlen(QUIT)) == 0 ) break;
+                    printf("Please input from keyboard and use enter to complete your input: \n");
+                }
+            }
+            
+        }
+
+        FD_ZERO(&readfds);
+        FD_SET(STDIN_FILENO, &readfds);
+    }
+}
