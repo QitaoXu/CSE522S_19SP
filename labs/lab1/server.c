1bbe314fa44aac8517b1d6069afd69acc9f6f65e
徐期涛 <xuqitao@qx-mbp.dhcp.wustl.edu>
2019-02-12 17:00:18 -0600

    debug

--- labs/lab1/server.c
+++ labs/lab1/server.c
@@ -151,6 +151,7 @@ int main( int argc, char *argv[] ) {
         j++;
         
     }
+    printf("Before while loop: j = %d\n", j);
 
     /* After all those above files are opened,
      * server needs to create a socket monitoring 
