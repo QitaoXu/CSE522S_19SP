c9af23933cc08e525721f818027a846dee8721d5
徐期涛 <xuqitao@qx-mbp.dhcp.wustl.edu>
2019-02-15 21:26:05 -0600

    avl tree interface

--- labs/lab1/tree.h
+++ labs/lab1/tree.h
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
 #include <stdlib.h>
 
 struct Node
@@ -12,28 +10,94 @@ struct Node
     int height;
 };
 
-int 
-max( int a, int b );
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
 
-int
-height( struct Node *N );
+int height( struct Node * N) {
+    if (N == NULL) return 0;
 
-struct Node*
-newNode( int key, char *line);
+    return N->height;
+}
 
-struct Node*
-rightrotate( struct Node *y );
+struct Node * newNode( int key, char * line ) {
+    struct Node * node = (struct Node *)malloc(sizeof(struct Node));
+    node->key = key;
+    node->left = NULL;
+    node->right = NULL;
+    node->line = line;
+    node->height = 1;
+    return node;
+}
 
-struct Node*
-leftRotate( struct Node *x );
+struct Node *rightRotate( struct Node * y) {
+    struct Node * x = y->left;
+    struct Node * T2 = x->right;
 
-int
-getBalance( struct Node *N);
+    x->right = y;
+    y->left = T2;
 
-struct Node*
-insert( struct Node * node, int key, char *line);
+    y->height = max(height(y->left), height(y->right)) + 1;
+    x->height = max(height(x->left), height(x->right)) + 1;
 
-void 
-preOrder( struct Node *root );
+    return x;
+}
 
+struct Node *leftRotate( struct Node * x) {
+    struct Node * y = x->right;
+    struct Node * T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    x->height = max(height(x->left), height(x->right)) + 1;
+    y->height = max(height(y->left), height(y->right)) + 1;
+
+    return y;
+}
+
+int getBalance( struct Node * N ) {
+    if (N == NULL) return 0;
+    return height(N->left) - height(N->right);
+}
+
+struct Node * insert( struct Node * node, int key, char * line) {
+    if (node == NULL) return newNode(key, line);
+
+    if (key < node->key)
+        node->left = insert(node->left, key, line);
+    else if (key > node->key)
+        node->right = insert(node->right, key, line);
+    node->height = 1 + max(height(node->left), height(node->right));
+
+    int balance = getBalance(node);
+
+    if (balance > 1 && key < node->left->key) { // left left case
+        return rightRotate(node);
+    }
+
+    if (balance < -1 && key > node->right->key ) { // right right case
+        return leftRotate(node);
+    }
+
+    if (balance > 1 && key > node->left->key ) { // left right case
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+
+    if (balance > -1 && key < node->right->key ) { // right left case
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+
+    return node;
+}
+
+void preOrder(struct Node * root) {
+    if (root != NULL) {
+        printf("%d:%s\n", root->key, root->line);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
 
