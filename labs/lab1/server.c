d203d17f97620060ae7c35c1b86131f065f5a7a7
徐期涛 <xuqitao@qx-mbp.dhcp.wustl.edu>
2019-02-17 19:46:06 -0600

    server develop

--- labs/lab1/server.c
+++ labs/lab1/server.c
@@ -290,6 +290,7 @@ int main( int argc, char *argv[] ) {
 
                     fds[m].events = POLLIN;
                     trackers[m - 1].is_sent = FINISHED;
+                    break;
                     
                 }
 
