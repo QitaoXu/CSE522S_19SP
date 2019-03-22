d00fded2cdc56c9647f9236449332ba48133336f
Zhe Wang <zhewang.kakaiu@yahoo.com>
2019-03-21 21:55:49 -0500

    fixed

--- labs/lab2_m/basic.h
+++ labs/lab2_m/basic.h
@@ -69,46 +69,7 @@ typedef struct Core Core;
 enum mode { RUN = 1, CALIBRATE };
 char * mode_input = "calibrate";
 module_param(mode_input, charp, 0);
-void parse_module_param() {
-	if (sysfs_streq(mode_input, "run")) {
-		mode = RUN;
-	} else {
-		mode = CALIBRATE;
-	} 
-} 
 
 //Make Thread Name
 char thread_name_base[256] = "thread";
-char* get_thread_name_s(char *str, int num){
-    char c;
-    int len = strlen(str);
-   	int digit;
-   	int i;
-   	int j=0;
-   	int base=10;
-   	int cop=num;
-   	int length=1;
-   	int square=1;
-   	while(cop>=base){
-   		cop/=base;
-   		length+=1;
-   	}
-   	i=length;
-   	while(i>0){
-   		j=0;
-   		square=1;
-		while(j<(i-1)){
-			square*=base;
-			j+=1;
-			
-		}
-		digit=num/(square)%base;
-   		c=digit+'0';
-   		str[len]=c;
-   		i-=1;
-   		len+=1;
-   	}
-    str[len+1] = '\0';
-	return str;
-}
 #endif /* BASIC_H */
