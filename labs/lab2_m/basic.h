08b0db1d7f34eca68daf1e2d01a73d9c14f30d50
Zhe Wang <zhewang.kakaiu@yahoo.com>
2019-03-21 21:53:35 -0500

    fixed

--- labs/lab2_m/basic.h
+++ labs/lab2_m/basic.h
@@ -20,26 +20,26 @@ static struct sched_param calibrate_param;
 
 //customized structures
 struct Task{
-	struct ktime_t period; /*period of task second*/
+	ktime_t period; /*period of task second*/
 	// struct subtask *subtasks; /* subtasks within the task struct */
 	struct list_head task_subtask_list;
 	int num;/* number of subtask */
 	int index; /* index of task */
 	int starting_index;/* starting index of subtask */
-	struct ktime_t execution_time; /*execution time of all subtask*/
+	ktime_t execution_time; /*execution time of all subtask*/
 };
 
 struct Subtask {
 	int index; /*index of subtask within the task struct*/
 	int core; /* on which core of your Raspberry Pi 3 each subtask should run, set core to -1 if no aviable core to run*/
-	Task *parent; /*parent task of subtask*/
+	struct Task *parent; /*parent task of subtask*/
 	int loop_count; /*init to 0 or Z+, */
 
-	struct ktime_t last_release_time; /*initialized to 0, record the last time the subtask was released*/
-	struct ktime_t cumul_exec_time;/* sum up the execution times of that subtask and all of its predecessors within the same task*/
+	ktime_t last_release_time; /*initialized to 0, record the last time the subtask was released*/
+	ktime_t cumul_exec_time;/* sum up the execution times of that subtask and all of its predecessors within the same task*/
 
 	float utilization; /*divide its execution time by its task's period.*/
-	struct ktime_t execution_time;/*execution time of subtask millisecond*/
+	ktime_t execution_time;/*execution time of subtask millisecond*/
 
 	struct task_struct *sub_thread; /*pointer to the task_struct*/
 	struct hrtimer hr_timer; /* timer for the subtask*/
@@ -47,7 +47,7 @@ struct Subtask {
 	struct subtask *prev; /* previous subtask within parent task struct*/
 
 	bool flag_sched; /*if the subtask is temporarily not available, */
-	struct ktime_t relative_ddl; /*task period* subtask's execution time/task's execution time*/
+	ktime_t relative_ddl; /*task period* subtask's execution time/task's execution time*/
 	struct sched_param schedule_param; /*priority of subtask on the core*/
 	struct list_head core_subtask_list;/*list head of the core on which the subtask is assigned */
 	struct list_head task_subtask_list;
