12a40379bc27f92591bba05eab529d5cc5f27247
Zhe Wang <zhewang.kakaiu@yahoo.com>
2019-03-21 23:38:47 -0500

    fixed

--- labs/lab2_m/module.c
+++ labs/lab2_m/module.c
@@ -108,8 +108,6 @@ static int init_run_subtask_fn(void * data){
 static int simple_init (void) {
 	int i, j, ret;
 	Core c;
-	struct sched_param schedule_param;
-	struct sched_param calibrate_param;
 	parse_module_param();
 
 	if(mode == RUN){
@@ -118,7 +116,7 @@ static int simple_init (void) {
 		for(i=0; i<num_subtask; i++){
 			subtasks[i].sub_thread = kthread_create(init_run_subtask_fn, (void *)(&subtasks[i]), get_thread_name_s(thread_name_base, i));
 			kthread_bind(subtasks[i].sub_thread, subtasks[i].core);
-			schedule_param = { .sched_priority=subtasks[i].sched_priori };
+			struct sched_param schedule_param = { .sched_priority=subtasks[i].sched_priori };
 			ret = sched_setscheduler(subtasks[i].sub_thread->pid, SCHED_FIFO, &schedule_param);
 			if (ret < 0) {
 				printk(KERN_INFO, "sched_setscheduler failed!");
@@ -140,14 +138,14 @@ static int simple_init (void) {
 			c = cores[j];
 			calibrate_kthreads[i] = kthread_create(calibrate_fn, (void *)(&subtasks[i]), sprintf("calibrate%d", i));
 			kthread_bind(calibrate_kthreads[i], i);
-			calibrate_param = { .sched_priority=1 }
+			struct sched_param calibrate_param = { .sched_priority=1 }
 			ret = sched_setscheduler(calibrate_kthreads[i]->pid, SCHED_FIFO, &calibrate_param);
 			if (ret < 0) {
 				printk(KERN_INFO, "sched_setscheduler failed!");
 				return -1;
 			}
 			for (i=0; i<c.num; j++) {
-				schedule_param = { .sched_priority=c.subtask_list[j]->sched_priori};
+				struct sched_param schedule_param = { .sched_priority=c.subtask_list[j]->sched_priori};
 				sched_setscheduler(c.subtask_list[j]->sub_thread->pid, SCHED_FIFO, &schedule_param);
 			}
 		}
