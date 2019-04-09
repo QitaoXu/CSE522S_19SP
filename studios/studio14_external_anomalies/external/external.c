d7fe6802898d15cecadae230131d9ebbaa473f62
徐期涛 <jeremyxuqitao@outlook.com>
2019-04-08 23:11:51 -0500

    studio14

---
+++ studios/studio14_external_anomalies/external/external.c
@@ -0,0 +1,38 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/kthread.h>
+#include <linux/hrtimer.h>
+#include <linux/ktime.h>
+#include <linux/sched.h> 
+#include <linux/time.h>
+#include <linux/hrtimer.h>
+#include <linux/types.h>
+
+static int kthread_fn(void *data) {
+
+    int *index = (int *)data;
+
+    printk(KERN_INFO "This is kthread: %s function!, index = %d\n", current->comm, *index);
+
+    return 0;
+}
+
+static int external_init (void) {
+
+    printk(KERN_ALERT "multithread module is being loaded!\n");
+
+    return 0;
+}
+
+static void external_exit (void) {
+
+    printk(KERN_ALERT "multithread module is being unloaded\n");
+}
+
+module_init (external_init);
+module_exit (external_exit);
+
+MODULE_LICENSE ("GPL");
+MODULE_AUTHOR ("Jiangnan Liu, Qitao Xu, Zhe Wang");
+MODULE_DESCRIPTION ("Observing external anomalies");
