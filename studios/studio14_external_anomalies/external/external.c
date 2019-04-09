41a9f4ddffcafee24c21af08bb8b13156aad0039
徐期涛 <jeremyxuqitao@outlook.com>
2019-04-09 10:57:46 -0500

    studio14

--- studios/studio14_external_anomalies/external/external.c
+++ studios/studio14_external_anomalies/external/external.c
@@ -8,6 +8,17 @@
 #include <linux/time.h>
 #include <linux/hrtimer.h>
 #include <linux/types.h>
+#include <linux/string.h>
+
+#define KTIME_ARR_SIZE 2
+
+struct sched_param {
+    int sched_priority;
+};
+
+ktime_t ktime_arr[KTIME_ARR_SIZE];
+struct hrtimer timer;
+struct task_struct *kthread;
 
 static char* policy = "RR";
 static int core_index = 0;
@@ -19,16 +30,37 @@ module_param(core_index, int, 0764);
 module_param(period, int, 0764);
 module_param(iter_num, int, 0764);
 
+static enum hrtimer_restart hrtimer_fn(struct hrtimer * timer) {
+
+    /* wake up kthread
+    */
+    wake_up_process(kthread);
+
+    printk(KERN_INFO "This is in hrtimer_fn!\n");
+
+    return HRTIMER_NORESTART;
+}
+
 static int kthread_fn(void *data) {
 
-    int *index = (int *)data;
+    int *iter = (int *)data;
 
-    printk(KERN_INFO "This is kthread: %s function!, index = %d\n", current->comm, *index);
+    printk(KERN_INFO "This is kthread: %s function!, iter_num = %d\n", current->comm, *iter);
+
+    ktime_arr[1] = get_ktime();
+
+    set_current_state(TASK_INTERRUPTIBLE);
+
+    schedule();
 
     return 0;
 }
 
 static int external_init (void) {
+
+   
+    struct sched_param param;
+    param.sched_priority = 5;
 
     printk(KERN_ALERT "multithread module is being loaded!\n");
 
@@ -37,12 +69,37 @@ static int external_init (void) {
     printk(KERN_INFO "period: %dms\n", period);
     printk(KERN_INFO "iter_num: %d\n", iter_num);
 
+    kthread = kthread_create(kthread_fn, &iter_num, "kthread");
+    kthread_bind(kthread, core_index);
+    sched_setscheduler(kthread, SCHED_RR, &param);
+
+    /* hrtimer init and start
+    */
+    hrtimer_init(&timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+    timer.function = &hrtimer_fn;
+
+    ktime_arr[0] = get_ktime();
+
+    while (iter_num > 0) {
+
+        hrtimer_start(&timer, ktime_set(0, period * 1000000), HRTIMER_MODE_REL);
+
+        iter_num -= 1;
+
+    }
+
     return 0;
 }
 
 static void external_exit (void) {
 
+    struct timespec diff;
+    diff = ktime_to_timespec(ktime_sub(ktime_arr[1], ktime_arr[0]));
+
+    printk(KERN_INFO "latency for creating kthread: %lld.%.9ld\n",  (long long)diff.tv_sec, diff.tv_nsec);
     printk(KERN_ALERT "multithread module is being unloaded\n");
+
+
 }
 
 module_init (external_init);
