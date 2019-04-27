32a1f58f01c893968d6d8422c0d622691e6495a2
Zhe Wang <zhewang.kakaiu@yahoo.com>
2019-04-27 01:22:32 -0500

    project fixed zhe

--- project/netlink.c
+++ project/netlink.c
@@ -12,223 +12,186 @@
 #include <linux/netlink.h>
 #include <linux/buffer_head.h>
 #include <net/net_namespace.h>
+#include <linux/string.h>
 #include <linux/slab.h>
+
 #define NETLINK_TEST 17 
-const char d[2] = " ";
-static struct sock *socket_ptr = NULL;
-static unsigned long period_sec= 1;
-static unsigned long period_nsec=0;
+#define BUFFER_SIZE 256
+#define UTIL_THRESHOLD 1250 // 80/100
+#define UTIL_PRECISION 1000
+
+static unsigned long period_sec = 1;
+static unsigned long period_nsec = 0;
+static unsigned long test = UTIL_THRESHOLD; //for test
+
+long prev_idle = 0;
+long prev_total = 0;
+
 static struct hrtimer hr_timer;
 static ktime_t interval;
-static struct task_struct *thread1;
-long prev_idle=0;
-long prev_total=0;
-// static char *statCPU="/proc/stat";
-struct sched_param{
+
+static struct task_struct *main_thread;
+
+static struct sock *socket_ptr = NULL;
+
+struct sched_param {
 	int sched_priority;
 };
-module_param(period_sec,ulong, 0);
+
+module_param(period_sec, ulong, 0);
 module_param(period_nsec, ulong, 0);
+module_param(test, ulong, 0);
 
 static void netlink_recv_msg(struct sk_buff * skb) {
-
     struct nlmsghdr * nlh = NULL;
-
     if (skb == NULL) {
         printk(KERN_INFO "skb is NULL \n");
-
-        return ;
+        return;
     }
-
     nlh = (struct nlmsghdr *)skb->data;
     printk(KERN_INFO "%s: received netlink message payload: %s\n", __FUNCTION__, (char *)NLMSG_DATA(nlh));
-
 }
 
 struct netlink_kernel_cfg cfg = {
     .input = netlink_recv_msg,
 };
-/* static function for kernel thread*/
 
-// struct file *file_open(const char *path, int flags, int rights) 
-// {
-//     struct file *filp = NULL;
-//     mm_segment_t oldfs;
-//     int err = 0;
-
-//     oldfs = get_fs();
-//     set_fs(get_ds());
-//     filp = filp_open(path, flags, rights);
-//     set_fs(oldfs);
-//     if (IS_ERR(filp)) {
-//         err = PTR_ERR(filp);
-//         return NULL;
-//     }
-//     return filp;
-// }
-// //Close a file (similar to close):
-
-// void file_close(struct file *file) 
-// {
-//     filp_close(file, NULL);
-// }
-// //Reading data from a file (similar to pread):
-
-// int file_read(struct file *file, unsigned long long offset, unsigned char *data, unsigned int size) 
-// {
-//     mm_segment_t oldfs;
-//     int ret;
-
-//     oldfs = get_fs();
-//     set_fs(get_ds());
-
-//     ret = vfs_read(file, data, size, &offset);
-
-//     set_fs(oldfs);
-//     return ret;
-// }   
-// //Writing data to a file (similar to pwrite):
-
-// int file_write(struct file *file, unsigned long long offset, unsigned char *data, unsigned int size) 
-// {
-//     mm_segment_t oldfs;
-//     int ret;
-
-//     oldfs = get_fs();
-//     set_fs(get_ds());
-
-//     ret = vfs_write(file, data, size, &offset);
-
-//     set_fs(oldfs);
-//     return ret;
-// }
-// //Syncing changes a file (similar to fsync):
-
-// int file_sync(struct file *file) 
-// {
-//     vfs_fsync(file, 0);
-//     return 0;
-// }
-
-
+/*
+input is utilization threshold
+return three stats: 
+	-1 	func running error
+	0	utilization is low
+	1	utilization is high
+*/
+//https://supportcenter.checkpoint.com/supportcenter/portal?eventSubmit_doGoviewsolutiondetails=&solutionid=sk65143
 //https://stackoverflow.com/questions/1184274/read-write-files-within-a-linux-kernel-module
-static int thread_fn(void * data){
-	int i;
+static int do_analysis_proc_stat(int threshold) {
 	struct file *f;
+	char buffer[BUFFER_SIZE] = {'\0'};
+	mm_segment_t fs;
+	long idle = 0;
+	long idlecpu = 0;
+	long iowait = 0;
+	long total = 0;
+	long totald = 0;
+	long idled = 0;
+	long split;
+	int i = 0;
 	int ret;
-    // char buf[1024];
-    char* token;
-    char* buffer;
-    long total;
-    long total_idle;
-    long idle;
-    long split;
-    long long percentage=0;
-    mm_segment_t fs;
+	char* token;
+	char *cur;
 
-    socket_ptr = netlink_kernel_create(&init_net, NETLINK_TEST, &cfg);
-    printk(KERN_INFO "Initializing Netlink Socket\n");
-    token=kmalloc(sizeof(char*),GFP_KERNEL);
-    if (!token)
-    	printk(KERN_ALERT "Allocation error!!.\n");
-    buffer=kmalloc_array(sizeof(char*),1024,GFP_KERNEL);
-    if (!buffer)
-    	printk(KERN_ALERT "Allocation error!!.\n");
-    // Init the buffer with 0
-    // memset(buf, 0, 1024);
-    // To see in /var/log/messages that the module is operating
-    printk(KERN_INFO "My module is loaded\n");
-    // I am using Fedora and for the test I have chosen following file
-    // Obviously it is much smaller than the 128 bytes, but hell with it =)
-    
+	f = filp_open("/proc/stat", O_RDONLY, 0);
+	if(!f){
+		printk(KERN_ALERT "filp_open error!!.\n");
+		filp_close(f, NULL);
+		return -1;
+	} else {
+		fs = get_fs();
+		set_fs(get_ds());
+		f->f_op->read(f, buffer, BUFFER_SIZE, &f->f_pos);
+		set_fs(fs);
+		filp_close(f, NULL);
+
+		cur = buffer;
+		while( (token = strsep(&cur, "  ")) != NULL &&i<9){
+			if(i==0||i==1){
+				i++;
+				continue;
+			} else {
+				//printk(KERN_INFO "%s %d", token, i);
+				ret = kstrtol(token, 10, &split);
+				if(ret!=0) {
+					printk(KERN_ALERT "Conversion1 error!!\n");
+					return -1;
+				}
+				total = total + split;
+
+				if(i==5){ //idle cpu time
+					ret = kstrtol(token, 10, &idlecpu);
+					if(ret!=0) {
+						printk(KERN_ALERT "Conversion2 error!!\n");
+						return -1;
+					}
+				}
+				if(i==6){ //i/o wait
+					ret = kstrtol(token, 10, &iowait);
+					if(ret!=0) {
+						printk(KERN_ALERT "Conversion2 error!!\n");
+						return -1;
+					}
+				}
+				i++;
+			}
+		}
+		idle = idlecpu + iowait;
+		totald = total - prev_total;
+		idled = idle - prev_idle;
+
+		printk(KERN_INFO "utilization = %lu/%lu", totald-idled, totald);
+
+		prev_total = total;
+		prev_idle = idle;
+
+		printk("1/utilization = %lu, threshold = %d (< leads to high util)", (totald)*UTIL_PRECISION/(totald-idled), threshold);
+		if ((totald)*UTIL_PRECISION/(totald-idled) < threshold) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+}
+
+static int thread_fn(void * data) {
+	int system_util_stat = -1;
 	while (!kthread_should_stop()){ 
 		set_current_state(TASK_INTERRUPTIBLE);
   		schedule();
-		f = filp_open("/proc/stat", O_RDONLY, 0);
-	    if(f == NULL)
-	        printk(KERN_ALERT "filp_open error!!.\n");
-	    else{
-	        // Get current segment descriptor
-	        fs = get_fs();
-	        // Set segment descriptor associated to kernel space
-	        set_fs(get_ds());
-	        // Read the file
-	        f->f_op->read(f, buffer, 1024, &f->f_pos);
-	        // Restore segment descriptor
-	        set_fs(fs);
-	        // See what we read from file
-	        printk(KERN_INFO "buf:%s",buffer);
-	    }
-	    filp_close(f,NULL);
-	    total=0;
-	    idle=0;
-	    i=0;
-	    percentage=0;
-	    total_idle=0;
-	    // buffer=&buf;
-	    while( (token = strsep(&buffer,d)) != NULL &&i<10){
-        	printk(KERN_INFO "%s %d \n",token,i);
-        	if(i!=0&&i!=1){
-        		ret=kstrtol(token,10,&split);
-    	 		if(ret!=0)
-    	 			printk(KERN_ALERT "Conversion error!!.\n");
-    	 		total=total+split;
-    	 	}
-    	 	
-    	 	if(i==5||i==6){
-    	 		ret=kstrtol(token,10,&idle);
-    	 		if(ret!=0)
-    	 			printk(KERN_ALERT "Conversion error!!.\n");
-    	 		total_idle+=idle;
-	    	}
-        	i+=1;
-	    }
-	   
-	    // percentage=(1.0 - (idle-prev_idle)*1.0/(total-prev_total))*100;
-	   	prev_idle=total_idle;
-	   	prev_total=total;
+		system_util_stat = do_analysis_proc_stat(test);
+		if (system_util_stat==-1) { //func running error
+			continue;
+		} else if (system_util_stat==0) {
+			printk(KERN_INFO "low utilization");
+			continue;
+		} else if (system_util_stat==1) {
+			printk(KERN_INFO "high utilization");
+			continue;
+			//TODO
 
-      	printk(KERN_INFO "In thread1 %ld %ld",idle,total);
+		} else {
+			printk(KERN_ALERT "wrong system_util_stat: %d", system_util_stat);
+		}
 	}
-	kfree(buffer);
-	kfree(token);
-	sock_release(socket_ptr->sk_socket);
-	//printk(KERN_INFO "In thread1");
-	// struct file *f;
-
-	
 	return 0;
 }
 
-
 /*timer expiration*/
-static enum hrtimer_restart timer_callback( struct hrtimer *timer_for_restart )
-{
+static enum hrtimer_restart timer_callback(struct hrtimer *timer_for_restart) {
 	ktime_t currtime;
-	wake_up_process(thread1);
+	wake_up_process(main_thread);
   	currtime  = ktime_get();
   	hrtimer_forward(timer_for_restart, currtime , interval);
-	// set_pin_value(PIO_G,9,(cnt++ & 1)); //Toggle LED 
 	return HRTIMER_RESTART;
 }
-/* init function - logs that initialization happened, returns success */
 
+/* init function - logs that initialization happened, returns success */
 static int simple_init (void) {
 	int ret;
-	char name[8]="thread1";
-	struct sched_param param= { . sched_priority=95};
-	
+	struct sched_param param= {.sched_priority=95};
+	socket_ptr = netlink_kernel_create(&init_net, NETLINK_TEST, &cfg);
+    printk(KERN_INFO "link created");
 	interval=ktime_set(period_sec,period_nsec);
 	hrtimer_init(&hr_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
 	hr_timer.function=&timer_callback;
-	thread1=kthread_create(thread_fn,NULL,name);
-	kthread_bind(thread1, 1);
-	ret=sched_setscheduler(thread1, SCHED_FIFO, &param);
+
+	main_thread = kthread_create(thread_fn, NULL ,"main_thread");
+	kthread_bind(main_thread, 1);
+	ret=sched_setscheduler(main_thread, SCHED_FIFO, &param);
 	if(ret==-1){
-  		printk(KERN_ALERT "sched_setscheduler");
+  		printk(KERN_ALERT "sched_setscheduler failed");
 		return 1;  
 	}
-	wake_up_process(thread1);
+	wake_up_process(main_thread);
 	hrtimer_start(&hr_timer,interval, HRTIMER_MODE_REL);
 
     printk(KERN_ALERT "simple module initialized\n");
@@ -238,8 +201,9 @@ static int simple_init (void) {
 /* exit function - logs that the module is being removed */
 static void simple_exit (void) {
 	int ret;
+	sock_release(socket_ptr->sk_socket);
 	hrtimer_cancel(&hr_timer);
- 	ret = kthread_stop(thread1);
+ 	ret = kthread_stop(main_thread);
  	if(!ret)
   		printk(KERN_INFO "Thread stopped");
     printk(KERN_ALERT "simple module is being unloaded\n");
